8178432c6f70364bc1237c8d09720067106b36bc
Damon George <damongeorge@Damons-MacBook-Pro.local>
2017-11-28 21:07:21 -0800

    Example of using ASCII Tables

--- src/main/java/config/Testing.java
+++ src/main/java/config/Testing.java
@@ -2,6 +2,10 @@ package config;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.ArrayTableModel;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
 
 @ShellComponent
 public class Testing {
@@ -9,5 +13,12 @@ public class Testing {
     @ShellMethod("Add two integers together.")
     public void add(int a, int b) {
         System.out.println( a + b);
+        
+        
+        String[][] array = {{"hey", "dog"}, {"whats", "up"}};
+        ArrayTableModel model = new ArrayTableModel(array);
+        TableBuilder builder = new TableBuilder(model);
+        builder.addFullBorder(BorderStyle.oldschool);
+        System.out.println(builder.build().render(50));
     }
 }
